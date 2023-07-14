7f3229c9c2e82c34f479e7d9c71b829efb80af04
Bassel Al Omari <baselomari@hotmail.com>
2023-07-13 22:45:13 -0400

    feat: add described network implementation

--- baselines/perfedavg/perfedavg/models.py
+++ baselines/perfedavg/perfedavg/models.py
@@ -1,7 +1,37 @@
-"""Define our models, and training and eval functions.
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from copy import deepcopy
+from torch.utils.data import DataLoader
 
-If your model is 100% off-the-shelf (e.g. directly from torchvision without requiring
-modifications) you might be better off instantiating your  model directly from the Hydra
-config. In this way, swapping your model for  another one can be done without changing
-the python code at all
-"""
+
+class Net(nn.Module):
+    """
+    Neural network architecture as described in section 5 (Numerical Experiments)
+    of the PerFedAvg paper
+    """
+
+    def __init__(self, input_size: int, num_classes: int) -> None:
+        super().__init__()
+        self.h1 = nn.Linear(input_size, 80)
+        self.h2 = nn.Linear(80, 60)
+        self.out = nn.Linear(60, num_classes)
+        
+
+    def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the CNN.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input Tensor that will pass through the network
+
+        Returns
+        -------
+        torch.Tensor
+            The resulting Tensor after it has passed through the network
+        """
+        z1 = F.elu(self.h1(input_tensor), alpha=0.2)
+        z2 = F.elu(self.h2(z1), alpha=0.2)
+        logits = F.elu(self.out(z2), alpha=0.2)
+        return logits
